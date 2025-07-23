73ae83cf7cbe9ad3abaf29e787d3759950bc9412
PeterJohn1234 <bhonemyintchain@gmail.com>
2025-07-24 02:18:22 +0700

    Third-Part Bhone Myint Chain

--- Assignment _1.php
+++ Assignment _1.php
@@ -32,6 +32,30 @@
         <label for="gender">Gender:</label>
         <input type="radio" id="male" name="gender" value="Male"> Male
         <input type="radio" id="female" name="gender" value="Female"> Female <br><br>
+
+        <label for="department">Department:</label><br>
+        <input type="checkbox" id="english"  name="department" value="English"> English
+        <input type="checkbox" id="computer" name="department" value="Computer"> Computer
+        <input type="checkbox" id="business" name="department" value="Business"> Business<br><br>
+        
+        <label for="course">Course:</label>
+        <select name="course" id="course"> 
+        <option value="select">Select Course</option>
+        <option value="HTML">HTML/CSS</option>
+        <option value="AI">AI</option>
+        <option value="PHP">PHP</option>
+        <option value="Networking">Networking</option>
+        <option value="Game">Game</option>
+        </select><br><br>
+
+        <label for="city">City:</label>
+        <input type="text" id="city" name="city"><br><br>
+
+
+        <label for="address">Address:</label><br>
+        <textarea id="address" name="address" rows="4" cols="30" ></textarea><br><br>
+
+        <input type="submit" value="Register" name="submit">
         </form>
 </body>
 </html>
